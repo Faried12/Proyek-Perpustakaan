842237dc4e1eacee54fbc1894207080d0e36eef0
1Ly45 <137293380+1Ly45@users.noreply.github.com>
2023-11-03 08:39:59 +0700

    Add files via upload

---
+++ perpustakaan.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Nov 2023 pada 02.37
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_anggota`
+--
+
+CREATE TABLE `data_anggota` (
+  `id_anggota` varchar(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `jenis_kelamin` tinyint(1) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_telpon` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `data_anggota`
+--
+
+INSERT INTO `data_anggota` (`id_anggota`, `nama`, `jenis_kelamin`, `tanggal_lahir`, `alamat`, `no_telpon`) VALUES
+('A000001', 'Bangun Panduko Johan', 1, '2004-05-12', 'Puncangan', '085921595619'),
+('A000002', 'Muhammad Farid Saputra', 1, '2004-01-12', 'Mahamhaji', '082268597785'),
+('A000003', 'Muhammad Ilyas', 1, '2004-03-27', 'Makamhaji', '087733625013');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_buku`
+--
+
+CREATE TABLE `data_buku` (
+  `id_buku` varchar(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `kategori` varchar(255) NOT NULL,
+  `penulis` varchar(255) NOT NULL,
+  `stock` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `data_buku`
+--
+
+INSERT INTO `data_buku` (`id_buku`, `nama`, `kategori`, `penulis`, `stock`) VALUES
+('B000001', 'One Piece 100', 'komik', 'Eiichiro Oda', 5),
+('B000002', 'Koala Kumal', 'Novel', 'Raditya Dika', 5),
+('B000003', 'Dasar-Dasar Teknik Informatika', 'Jurnal', 'Novega Pratama Adiputra', 3),
+('B000004', 'Berserk vol.1', 'Komik', 'Kentaro Miura', 5),
+('B000005', 'Laskar Pelangi', 'novel', 'Andrea Hirata', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` varchar(11) NOT NULL,
+  `tanggal_peminjaman` date NOT NULL,
+  `id_anggota` varchar(11) NOT NULL,
+  `id_buku` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `data_anggota`
+--
+ALTER TABLE `data_anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indeks untuk tabel `data_buku`
+--
+ALTER TABLE `data_buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`,`id_anggota`,`id_buku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
